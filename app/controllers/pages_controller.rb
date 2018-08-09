60f0a1028a0716d3ebeb4c2615852f7534254f39
khris <khris56@hotmail.com>
2018-08-08 20:56:11 -0500

    rutas al path

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,19 @@
 class PagesController < ApplicationController
   def home
   end
+#about_us ruta , metodo y vista q se llame igual al metodo
+
+def about_us
+  @greeting = "Lorem ipsum dolor"
+end
+
+#contact   direccion y telefono ,
+def contact
+  @contact = ["Zorro arango", 3504500059]
+end
+
+#posts array de strings
+def posts
+  @posts = ["publicacion 1", "publicacion 2"]
+end
 end
